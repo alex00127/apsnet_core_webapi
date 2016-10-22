0b2260e6f6abbf16dcb842d8f1039362713e3f1f
Alex Chiu <alex00127@hotmail.com>
2016-10-22 18:49:39 +0800

    create webapi prodject and complete CRUD

---
+++ Models/ITodoRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebAPI.Models
+{
+    public interface ITodoRepository
+    {
+        void Add(TodoItem item);
+        IEnumerable<TodoItem> GetAll();
+        TodoItem Find(string key);
+        TodoItem Remove(string key);
+        void Update(TodoItem item);
+    }
+}
